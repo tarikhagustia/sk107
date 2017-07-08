bcf510a1d03e9bba489a40a2e2a054140ab058bf
Verry Haryanto <alterme21@yahoo.com>
2017-07-08 17:05:07 +0700

    tampilan page demo account

--- resources/views/admin/account/open-demo-account.blade.php
+++ resources/views/admin/account/open-demo-account.blade.php
@@ -9,7 +9,7 @@
       <div class="panel">
         <div class="panel-body container-fluid">
           <div class="row row-lg">
-            <div class="col-md-6">
+            <div class="col-md-12">
               <!-- Example Basic -->
               <div class="example-wrap">
                 <h4 class="example-title">Demo Account List</h4>
@@ -20,69 +20,28 @@
                         <th>No</th>
                         <th>Username</th>
                         <th>Password</th>
-                        <th>Request Date</th>
-                        <th>Activated Date</th>
+                        <th>Request Time</th>
+                        <th>Activated Time</th>
 						<th>Status</th>
                       </tr>
                     </thead>
                     <tbody>
                       <tr>
                         <td>1</td>
-                        <td>Teagan</td>
-                        <td>Prohaska</td>
-                        <td>@Elijah</td>
-                        <td>
-                          <span class="label label-danger">admin</span>
-                        </td>
-                      </tr>
-                      <tr>
-                        <td>2</td>
-                        <td>Andy</td>
-                        <td>Gaylord</td>
-                        <td>@Ramiro</td>
-                        <td>
-                          <span class="label label-info">member</span>
-                        </td>
-                      </tr>
-                      <tr>
-                        <td>3</td>
-                        <td>Veronica</td>
-                        <td>Gusikowski</td>
-                        <td>@Maxime</td>
-                        <td>
-                          <span class="label label-warning">developer</span>
-                        </td>
-                      </tr>
-                      <tr>
-                        <td>4</td>
-                        <td>Bruce</td>
-                        <td>Rogahn</td>
-                        <td>@Maggio</td>
-                        <td>
-                          <span class="label label-success">supporter</span>
-                        </td>
-                      </tr>
-                      <tr>
-                        <td>5</td>
-                        <td>Carolina</td>
-                        <td>Hickle</td>
-                        <td>@Hammes</td>
-                        <td>
-                          <span class="label label-info">member</span>
-                        </td>
-                      </tr>
-                      <tr>
-                        <td>6</td>
-                        <td>Madaline</td>
-                        <td>Eichmann</td>
-                        <td>@Amaya</td>
-                        <td>
-                          <span class="label label-success">supporter</span>
-                        </td>
+                        <td>1102011022</td>
+                        <td>John347</td>
+                        <td>Jul 8, 2017, 11:07:24</td>
+                        <td>Jul 8, 2017, 11:07:27</td>
+						<td>
+						  <span class="label label-success">Active</span>
+						</td>
                       </tr>
                     </tbody>
                   </table>
                 </div>
+				<div class="col-md-2">
+			      <button type="submit" class="btn btn-primary btn-block">Request New Account</button>
+			    </div>
               </div>
               <!-- End Example Basic -->
             </div>
