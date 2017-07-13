a59bb40e68b16b74b630016047fcfd17875e6861
tarikhagustia <agustia.tarikh150@gmail.com>
2017-07-13 17:32:27 +0700

    form sk

---
+++ app/Http/Controllers/Admin/Account/FormController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Admin\Account;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class FormController extends Controller
+{
+    public function bpk_03()
+    {
+      return view('admin.account.form.107_pbk_03');
+    }
+}
