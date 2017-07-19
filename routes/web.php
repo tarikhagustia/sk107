3e9bf0b1b6df1c6a0a849c1d1975543f19e8891d
Verry Haryanto <alterme21@yahoo.com>
2017-07-19 10:20:38 +0700

    update view account real pbk 02.1 dan pbk 02.2

--- routes/web.php
+++ routes/web.php
@@ -66,13 +66,8 @@ Route::group(['middleware' => ['auth','admin']], function(){
 
   Route::get('admin/account/real-account/form/107-PBK-01', 'Admin\Account\FormController@bpk_01')->name('form.sk107.bpk.01');
   Route::get('admin/account/real-account/form/107-PBK-03', 'Admin\Account\FormController@bpk_03')->name('form.sk107.bpk.03');
-  Route::get('/admin/account/step2', function () {
-      return view('admin.account.step2');
-  });
-  Route::get('/admin/account/step3', function () {
-      return view('admin.account.step3');
-  });
-
+  Route::get('admin/account/real-account/form/107-PBK-02-1', 'Admin\Account\FormController@bpk_02_1')->name('form.sk107.bpk.02.1');
+  Route::get('admin/account/real-account/form/107-PBK-02-2', 'Admin\Account\FormController@bpk_02_2')->name('form.sk107.bpk.02.2');
 
   Route::group(['prefix' => 'services'], function(){
 
